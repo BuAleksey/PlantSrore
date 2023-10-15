264e270a6fff188cec74269f6cad650d1e0b25d3
Aleksey <oldf03@gmail.com>
2023-10-15 19:05:22 +0300

    Initial Commit

---
+++ PlantSrore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlantSrore
+//
+//  Created by Buba on 15.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
